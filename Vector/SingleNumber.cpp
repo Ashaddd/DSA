188e7258cd271901928436566f3ef12a40bea32e
Ashad Ali <mohammadashadali0786@gmail.com>
2026-01-26 22:58:36 +0530

    Single number

---
+++ Vector/SingleNumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> vec = {4, 2, 1, 2, 1};
+    int ans = 0;
+    for (int val : vec)
+    {
+        ans = ans ^ val;
+    }
+    cout << "the single number:" << ans;
+}
