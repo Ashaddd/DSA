841d12d361509c52e981b9f06d8a7b9397cff0fb
Ashad Ali <mohammadashadali0786@gmail.com>
2026-01-29 23:22:26 +0530

    Optimal (two pointer Technique)

--- ARRAY/PairSumOptimal.cpp
+++ ARRAY/PairSumOptimal.cpp
@@ -1,5 +1,27 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    int Arr
+int main()
+{
+    vector<int> vec = {2, 7, 11, 15};
+    int target = 13;
+    int n = vec.size();
+    int i = 0, j = n - 1;
+    while (i < j)
+    {
+        int pairSum = vec[i] + vec[j];
+        if (pairSum < target)
+        {
+            i++;
+        }
+        else if (pairSum > target)
+        {
+            j--;
+        }
+        else if (pairSum == target)
+        {
+            cout << i << "," << j;
+            break;
+        }
+    }
 }
