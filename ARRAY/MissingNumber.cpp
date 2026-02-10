93934c9d44b5a1447d9fe889630bf6c3e03f3067
Ashad Ali <mohammadashadali0786@gmail.com>
2026-02-10 23:03:27 +0530

    Missing Number from[0, n]

---
+++ ARRAY/MissingNumber.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int missingNumber(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != i)
+            return i;
+    }
+    return nums.size();
+}
+
+int main(){
+    vector<int>nums={3,0,2};
+    cout<<missingNumber(nums);
+}
